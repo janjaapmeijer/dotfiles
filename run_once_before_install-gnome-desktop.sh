95ffc21e44c3075d30e3401c1487fbda04686946
Jan Jaap Meijer <janjaapmeijer@gmail.com>
2024-01-10 18:01:57 +1100

    Script to install GNOME desktop.

---
+++ run_once_before_install-gnome-desktop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt install vanilla-gnome-desktop vanilla-gnome-default-settings
+sudo apt purge ubuntu-desktop gnome-shell-extension-ubuntu-dock
+
+sudo apt autoremove --purge
+
+sudo reboot
