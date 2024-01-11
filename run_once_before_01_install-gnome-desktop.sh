cc2685074496d3ad553bc99bcc68457e91fc82ae
Jan Jaap Meijer <janjaapmeijer@gmail.com>
2024-01-11 17:30:12 +1100

    Renaming run files.

--- run_once_before_01_install-gnome-desktop.sh
+++ run_once_before_01_install-gnome-desktop.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt upgrade
+sudo apt update && sudo apt upgrade
 
 sudo apt install vanilla-gnome-desktop vanilla-gnome-default-settings
 sudo apt purge ubuntu-desktop gnome-shell-extension-ubuntu-dock
