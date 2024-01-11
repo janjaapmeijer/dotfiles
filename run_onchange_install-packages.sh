ab2cb276978985f25b04838e8e2bd250e42e6c72
Jan Jaap Meijer <janjaapmeijer@gmail.com>
2024-01-11 15:53:31 +1100

    Simplify install packages.

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,16 +1,20 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install curl
+#!/bin/bash
+
+# Script to install Debian applications for new machine
+sudo apt update && sudo apt upgrade
+sudo apt install \
+  curl \
+  filezilla
 
 # install brave
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt install brave-browser
+# sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+# echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+# sudo apt install brave-browser
 
 # atom
 # (blender)
-sudo apt install calibre
-sudo apt install digikam
+# sudo apt install calibre
+# sudo apt install digikam
 # double commander or filezilla
 # eternl
 # guake terminal
