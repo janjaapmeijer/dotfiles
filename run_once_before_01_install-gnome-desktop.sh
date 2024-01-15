78f8d91b04fb92f25d05dfb656479931df25a3d9
Jan Jaap Meijer <janjaapmeijer@users.noreply.github.com>
2024-01-15 17:51:27 +1100

    Update run_once_before_01_install-gnome-desktop.sh
    Add git and curl packages.

--- run_once_before_01_install-gnome-desktop.sh
+++ run_once_before_01_install-gnome-desktop.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sudo apt update && sudo apt upgrade
+sudo apt git
+sudo apt curl
 
 sudo apt install vanilla-gnome-desktop vanilla-gnome-default-settings
 sudo apt purge ubuntu-desktop gnome-shell-extension-ubuntu-dock
